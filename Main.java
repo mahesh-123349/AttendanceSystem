478799c3a36cda46cedd0c93b9eb6ba850081c3a
mahesh-123349 <maheshrajanking@gmail.com>
2024-10-09 11:36:18 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package Attendance;
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args)throws SQLException {
+		// TODO Auto-generated method stub
+		Login login=new login();
+		login.loginView();
+
+	}
+
+}
